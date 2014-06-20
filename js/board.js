75806c7c7d8045430ab9e45abc8c5572a8eb1982
Nathan Rugg <nmrugg@gmail.com>
2014-06-20 15:32:04 +0800

    A board shows up.

---
+++ js/board.js
@@ -0,0 +1,129 @@
+var BOARD = function board_init(el, options)
+{
+    "use strict";
+    
+    var pieces,
+        board,
+        board_details = {
+            ranks: 8,
+            files: 8
+        },
+        squares,
+        pos;
+    
+    function check_el(el)
+    {
+        if (typeof el === "string") {
+            return document.getElementById(el);
+        }
+        return el;
+    }
+    
+    function get_init_pos()
+    {
+        ///NOTE: I made this a function so that we could pass other arguments, like chess varients.
+        return "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    }
+    
+    function make_square(x, y)
+    {
+        var el = document.createElement("div");
+        
+        el.classList.add("square");
+        el.classList.add("rank" + y);
+        el.classList.add("file" + x);
+        
+        if ((x + y) % 2) {
+            el.classList.add("light");
+        } else {
+            el.classList.add("dark");
+        }
+        
+        ///TODO: attach events
+        
+        return el;
+    }
+    
+    function make_rank(num)
+    {
+        var el = document.createElement("div");
+        
+        el.classList.add("rank");
+        el.classList.add("rank" + num);
+        
+        return el;
+    }
+    
+    function size_board(w, h)
+    {
+        board_details.width = parseFloat(w);
+        board_details.height = parseFloat(h);
+        
+        el.style.width  = board_details.width + "px";
+        el.style.height = board_details.height + "px";
+    }
+    
+    function create_board(new_el, dim)
+    {
+        var x,
+            y,
+            i = 0,
+            cur_rank;
+        
+        if (new_el) {
+            el = check_el(new_el);
+        }
+        
+        el.innerHTML = "";
+        
+        if (dim) {
+            size_board(dim.w, dim.h);
+        } else {
+            size_board(600, 600);
+        }
+        
+        squares = [];
+        pieces = [];
+        
+        for (y = board_details.ranks - 1; y >= 0; y -= 1) {
+            for (x = 0; x < board_details.files; x += 1) {
+                squares[i] = make_square(x, y);
+                if (x === 0) {
+                    cur_rank = make_rank(y);
+                    el.appendChild(cur_rank);
+                }
+                cur_rank.appendChild(squares[i]);
+                i += 1;
+            }
+        }
+        
+        el.classList.add("chess_board");
+        
+        return board;
+    }
+    
+    function set_board()
+    {
+        
+    }
+    
+    options = options || {};
+    
+    el = check_el(el);
+    
+    if (!options.pos) {
+        pos = get_init_pos();
+    }
+    
+    create_board(el, options.dim);
+    
+    set_board();
+    
+    board = {
+        create_board: create_board,
+        set_board: set_board,
+        size_board: size_board,
+    };
+    
+    return board;
+};
---
+++ js/kingdom.js
@@ -0,0 +1,7 @@
+(function ()
+{
+    "use strict";
+    
+    var board = BOARD("board");
+    
+}());
