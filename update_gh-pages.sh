13b46604e07e79bd8f7315d5769d025d811bc5b3
Nathan Rugg <nmrugg@gmail.com>
2015-05-31 15:53:56 +0800

    Copy girdle too.

--- update_gh-pages.sh
+++ update_gh-pages.sh
@@ -2,15 +2,18 @@ rm -Rf /tmp/git-gh-pages-TMP && \
 mkdir -p /tmp/git-gh-pages-TMP/css && \
 mkdir -p /tmp/git-gh-pages-TMP/img && \
 mkdir -p /tmp/git-gh-pages-TMP/js && \
+mkdir -p /tmp/git-gh-pages-TMP/node_modules/girdle && \
 cp -r css/*   /tmp/git-gh-pages-TMP/css/ && \
 cp -r img/* /tmp/git-gh-pages-TMP/img/ && \
 cp -r js/* /tmp/git-gh-pages-TMP/js/ && \
+cp -r node_modules/girdle/* /tmp/git-gh-pages-TMP/node_modules/girdle/ && \
 cp index.html /tmp/git-gh-pages-TMP/ && \
 cp favicon.ico /tmp/git-gh-pages-TMP/ && \
 git checkout gh-pages && \
 cp -r /tmp/git-gh-pages-TMP/css ./ && \
 cp -r /tmp/git-gh-pages-TMP/img ./ && \
 cp -r /tmp/git-gh-pages-TMP/js ./ && \
+cp -r /tmp/git-gh-pages-TMP/node_modules/girdle ./ && \
 cp /tmp/git-gh-pages-TMP/index.html ./ && \
 cp /tmp/git-gh-pages-TMP/favicon.ico ./ && \
 rm -R /tmp/git-gh-pages-TMP/ && \
