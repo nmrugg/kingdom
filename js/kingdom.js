4ea30f77051c454031283c6e0d8d1e63c362a363
Nathan Rugg <nmrugg@gmail.com>
2015-06-14 14:46:08 +0800

    Give 'em a little more time.

--- js/kingdom.js
+++ js/kingdom.js
@@ -617,7 +617,7 @@
     {
         ///NOTE: Without time, it thinks really fast. So, we give it a something to make it move reasonably quickly.
         ///      This time is also tweaked based on the level.
-        var default_time = 1000 * 60, /// 1 minute
+        var default_time = 1200 * 60, /// 1 minute
             wtime,
             btime,
             depth,
