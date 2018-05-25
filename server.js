28eade2ad913e73c805b8f260519d522ebd53535
Nathan Rugg <nmrugg@gmail.com>
2018-05-25 15:59:04 +0800

    fix some server issues

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ var http = require("http"),
     fs   = require("fs"),
     port = process.argv[2] || 9864; /// Defaults to port 9864
 
+function parse_range(headers, total)
+{
+    var match;
+    var start;
+    var end;
+    if (headers && headers.range && typeof headers.range === "string" && total) {
+        match = headers.range.match(/bytes=(\d+)(?:-(\d+))?/) || [];
+        start = match[1] && match[1] >= 0 ? Number(match[1]) : 0;
+        end = match[2] && match[2] > start && match[2] < total ? Number(match[2]) : total - 1;
+        return {
+            start: start,
+            end: end
+        };
+    }
+}
+
 function get_mime(filename)
 {
     var ext = path.extname(filename);
@@ -60,41 +76,56 @@ http.createServer(function (request, response)
         return;
     }
     
-    fs.exists(filename, function (exists)
+    function sendFile(filename)
     {
-        /// If the URI does not exist, display a 404 error.
-        if (!exists) {
-            response.writeHead(404, {"Content-Type": "text/plain"});
-            response.write("404 Not Found\n");
-            response.end();
-            return;
-        }
-        
-        /// If the URI is a directory, try to load index.html.
-        if (fs.statSync(filename).isDirectory()) {
-            filename += "/index.html";
-        }
-        
-        fs.readFile(filename, "binary", function (err, file)
+        fs.stat(filename, function (err, stats)
         {
-            var mime;
+            var responseHeaders = {};
+            var range;
+            var streamOptions = {"bufferSize": 4096};
+            var code = 200;
             
-            /// If the file cannot be loaded, display a 500 error.
-            if (err) {
-                response.writeHead(500, {"Content-Type": "text/plain"});
-                response.write(err + "\n");
-                response.end();
-                return;
+            if (!err && stats.isDirectory()) {
+                filename += "/index.html";
+                return sendFile(filename);
             }
             
-            mime = get_mime(filename);
-            
-            /// If the file loads correctly, write it to the client.
-            response.writeHead(200, mime ? {"Content-Type": mime} : undefined);
-            response.write(file, "binary");
-            response.end();
+            fs.exists(filename, function(exists) {
+                if (!exists) {
+                    response.writeHead(404, {"Content-Type": "text/plain"});
+                    response.write("404 Not Found\n");
+                    response.end();
+                    return;
+                }
+                
+                responseHeaders["Content-Type"] = get_mime(filename);
+                responseHeaders["Content-Length"] = stats.size;
+                responseHeaders["Accept-Ranges"] = "bytes";
+                
+                range = parse_range(request.headers, stats.size);
+                
+                if (range) {
+                    if (range.end <= range.start) {
+                        /// Range not satisfiable.
+                        response.writeHead(416);
+                        response.end();
+                        return;
+                    }
+                    streamOptions.start = range.start;
+                    streamOptions.end = range.end;
+                    responseHeaders["Content-Range"] = "bytes " + range.start + '-' + range.end + '/' + stats.size;
+                    code = 206;
+                }
+                
+                response.writeHead(code, responseHeaders);
+                
+                /// Stream the data out to prevent massive buffers on large files.
+                fs.createReadStream(filename, streamOptions).pipe(response);
+            });
         });
-    });
+    }
+    
+    sendFile(filename)
 }).listen(parseInt(port, 10));
 
 console.log("http://localhost:" + port + "/\nCTRL + C to shutdown");
